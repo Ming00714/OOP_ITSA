8cfb07876a5e07f35307c3cf96b0443430cbb328
Ming00714 <163003004+Ming00714@users.noreply.github.com>
2025-06-13 16:35:19 +0800

    Update Problem_04.cpp

--- 02算術運算/Problem_04.cpp
+++ 02算術運算/Problem_04.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    int n, a, b;
+    int n,a,b;
     cin >> n;
-    for(int i = 0; i < n; i++) {
+    for(int i=0; i<n; i++) {
         cin >> a >> b;
-        cout << a + b << endl;
+        cout << a+b << endl;
     }
     return 0;
 }
