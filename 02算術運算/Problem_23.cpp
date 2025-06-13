8f057162c907484ca37cc4ecf4d5de734fb859c5
Ming00714 <163003004+Ming00714@users.noreply.github.com>
2025-06-13 18:18:06 +0800

    Create Problem_23.cpp

---
+++ 02算術運算/Problem_23.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip> // for hex
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << hex << uppercase << n << endl; 
+    return 0;
+}
+
+// 十進位轉十六進位
