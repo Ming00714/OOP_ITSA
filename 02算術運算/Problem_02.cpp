551b7c8d00d8a9d44c13a02a29722d44004c6abb
Ming00714 <163003004+Ming00714@users.noreply.github.com>
2025-06-13 16:33:58 +0800

    Update Problem_02.cpp

--- 02算術運算/Problem_02.cpp
+++ 02算術運算/Problem_02.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
 
 int main() {
-    float a, b, c, area;
+    float a,b,c,area;
     cin >> a >> b >> c;
     area = (a+b)*c/2;
     
-    cout << "Trapezoid area:" << fixed << setprecision(1) << area << "\n";
+    cout << "Trapezoid area:" << fixed << setprecision(1) << area << endl;
 
     return 0;
 }
